fe7fbb7e3617834c35824a1177f00b14800b7468
edom18 <edo.m18@gmail.com>
2015-10-29 09:32:06 +0900

    Rename viewer class name.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    class Node {
+    class NodeView {
         constructor(id, point) {
             var radius = 15;
 
@@ -11,21 +11,6 @@
 
             this._dispatcher = new Dispatcher();
         }
-        // addNode(targetNode, cost) {
-        //     var edgeModel = this.createEdge(targetNode);
-        //     edgeModel.cost = cost;
-
-        //     targetNode.connect(this);
-        // }
-        // createEdge(targetNode) {
-        //     var edgeModel = EdgeModelManager.getInstance().create(this, targetNode);
-        //     this.model.addEdge(edgeModel);
-        //     return edgeModel;
-        // }
-        // connect(connectedNode) {
-        //     var edgeModel = EdgeModelManager.getInstance().create(this, connectedNode);
-        //     this.model.addEdge(edgeModel);
-        // }
         addListener(listener) {
             var type = listener.type;
             this.shape.addListener(new Listener(type, (target) => {
@@ -55,7 +40,7 @@
         }
     }
 
-    class Edge {
+    class EdgeView {
         constructor(model) {
             this.model = model;
             var nodeA = model.nodeA;
@@ -115,13 +100,14 @@
             var node5Point = new Point(150, 310);
             var node6Point = new Point(200, 140);
 
-            var node1 = new Node(1, node1Point); // start
-            var node2 = new Node(2, node2Point); // top
-            var node3 = new Node(3, node3Point); // center
-            var node4 = new Node(4, node4Point); // bottom-left
-            var node5 = new Node(5, node5Point); // bottom-right
-            var node6 = new Node(6, node6Point); // goal
+            var node1 = new NodeView(1, node1Point); // start
+            var node2 = new NodeView(2, node2Point); // top
+            var node3 = new NodeView(3, node3Point); // center
+            var node4 = new NodeView(4, node4Point); // bottom-left
+            var node5 = new NodeView(5, node5Point); // bottom-right
+            var node6 = new NodeView(6, node6Point); // goal
 
+            // Connect each nodes.
             var edgeModelManager = EdgeModelManager.getInstance();
             edgeModelManager.connect(node1.model, node2.model, 5);
             edgeModelManager.connect(node1.model, node3.model, 4);
@@ -137,21 +123,6 @@
 
             edgeModelManager.connect(node5.model, node6.model, 4);
 
-            // Connect each nodes.
-            // node1.addNode(node2, 5);
-            // node1.addNode(node3, 4);
-            // node1.addNode(node4, 2);
-
-            // node2.addNode(node6, 6);
-            // node2.addNode(node3, 2);
-
-            // node3.addNode(node4, 3);
-            // node3.addNode(node5, 2);
-
-            // node4.addNode(node5, 6);
-
-            // node5.addNode(node6, 4);
-
             return [
                 node1, node2, node3, node4, node5, node6
             ];
@@ -160,7 +131,7 @@
         launch() {
             var nodes = this.createNodes();
             // EdgeModelManager.getInstance().edges.forEach((edgeModel, i) => {
-            //     var edge = new Edge(edgeModel);
+            //     var edge = new EdgeView(edgeModel);
             //     edge.addToScene(this.scene);
             //     edge.addListener(new Listener('click', (target) => {
             //         this.inspector.selectedItem = target.model;
