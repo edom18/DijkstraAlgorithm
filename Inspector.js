3d8b745187f00a1be978aff1082de183fac51821
edom18 <edo.m18@gmail.com>
2015-10-30 00:51:51 +0900

    Implement some events for the files.

--- Inspector.js
+++ Inspector.js
@@ -6,15 +6,15 @@
             this._renderer = null;
             this._model    = null;
             this._element  = document.getElementById('inspector');
-
-            this._element.addEventListener('click', (evt) => {
-                this._model.set('cost', 10);
-            }, false);
         }
         set selectedItem(model) {
             this._model = model;
+            if (this._renderer) {
+                this._renderer.dispose();
+            }
             this._renderer = this.choiseRenderer(model);
-            this.update();
+            this._renderer.element = this._element;
+            this._renderer.render();
         }
         choiseRenderer(model) {
             if (model.type === 'edge') {
@@ -24,33 +24,105 @@
                 return new NodeModelRenderer(model);
             }
         }
-        update() {
-            this._renderer.update(this._element);
-        }
         render() {
-            this._renderer.render(this._element);
+            this._renderer.render();
         }
     }
 
     class ModelRenderer {
         constructor(model) {
-            this._model = model;
+            this._model   = model;
+            this._element = null;
         }
-        update(element) {
-            element.innerHTML = '';
-            this.render(element);
+        render() { }
+        setupEvents() { }
+        clearEvents() { }
+
+        set element(value) {
+            if (this._element === value) {
+                return;
+            }
+
+            this.clearEvents();
+            this._element = value;
+
+            this.update();
+            this.setupEvents();
         }
-        render(element) {
-            element.innerHTML = `Type: ${this._model.type}, ID: ${this._model.id}`;
+        get element() {
+            return this._element;
+        }
+
+        dispose() {
+            this.clearEvents();
         }
     }
 
     class NodeModelRenderer extends ModelRenderer {
+        constructor(model) {
+            super(model);
+
+            this.updateHandler = this._updateHandler.bind(this);
+        }
+
+        clearEvents() {
+            if (!this.element) {
+                return;
+            }
+            this._updateButton.removeEventListener('click', this.updateHandler, false);
+        }
+
+        update() {
+            this._typeField    = this.element.querySelector('.typeField');
+            this._costField    = this.element.querySelector('.costField');
+            this._updateButton = this.element.querySelector('.updateButton');
+        }
+
+        _updateHandler(evt) {
+            console.log(this);
+        }
+
+        setupEvents() {
+            this._updateButton.addEventListener('click', this.updateHandler, false);
+        }
+        render() {
+            this._typeField.innerHTML = this._model.type;
+            this._costField.innerHTML = this._model.cost;
+        }
     }
 
     class EdgeModelRenderer extends ModelRenderer {
-        render(element) {
-            element.innerHTML = 'hoge';
+        constructor(model) {
+            super(model);
+
+            this.updateHandler = this._updateHandler.bind(this);
+        }
+
+        clearEvents() {
+            if (!this.element) {
+                return;
+            }
+            this._updateButton.removeEventListener('click', this.updateHandler, false);
+        }
+
+        _updateHandler(evt) {
+            var cost = +this._costField.value;
+            this._model.set('cost', cost);
+        }
+
+        update() {
+            this._typeField    = this.element.querySelector('.typeField');
+            this._costField    = this.element.querySelector('.costField');
+            this._updateButton = this.element.querySelector('.updateButton');
+        }
+
+        setupEvents() {
+            this._updateButton.addEventListener('click', this.updateHandler, false);
+        }
+
+        render() {
+            this._typeField.innerHTML = this._model.type;
+            this._costField.value     = this._model.cost;
         }
     }
 
